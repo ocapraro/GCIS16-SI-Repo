b8958784266bd2ff428cac87bd7eb491999babb7
Oscar Capraro <oscar@mcapraro.com>
2023-09-14 09:33:38 -0400

    update for student solution

--- session3B.py
+++ session3B.py
@@ -11,22 +11,20 @@ def student_sorter(s1, s2):
     return s2+" "+s1
 
 def letter_or_number(character):
-  if(ord(character)<=57):
+  if(character<=chr(57)):
     return "Number"
-  if(ord(character)>=65):
+  if(character>=chr(65)):
     return "Letter"
 
-def random_letter(capital):
-  if(capital):
-    return(chr(random.randint(65,90)))
-  return(chr(random.randint(97,122)))
+# def random_letter(capital):
+#   if(capital):
+#     return(chr(random.randint(65,90)))
+#   return(chr(random.randint(97,122)))
   
 def main():
   print(student_sorter("Bob", "Alice"))
   print(letter_or_number("c"))
   print(letter_or_number("3"))
-  print(random_letter(True))
-  print(random_letter(False))
 
 if __name__ == "__main__":
   main()
