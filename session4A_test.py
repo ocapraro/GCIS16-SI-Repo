08489003976e5ba932ed8aac1996cb490cb89099
Oscar Capraro <oscar@mcapraro.com>
2023-09-18 21:17:23 -0400

    post session

--- session4A_test.py
+++ session4A_test.py
@@ -1,4 +1,20 @@
 """
 A test file for session4A.py
 author: Oscar Capraro
+seed:1
+output:E, S, Z, Y
 """
+import session4A
+import random
+
+def test_random_letter_lower():
+  # Setup
+  random.seed(1)
+  capital = False
+  expected = "e"
+
+  # Invoke
+  actual = session4A.random_letter(capital)
+
+  # Analyze
+  assert(expected == actual)
